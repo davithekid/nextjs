42719b48ca9d736356ceb103967f164083caa1a9
davithekid <byjdrk.27@gmail.com>
2025-03-25 00:40:40 -0300

    map

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -1,5 +1,31 @@
 import "./page.css"
 export default function Home() {
+
+  const artistas = [
+    { id: 1, nome: "Playboi Carti", img: "/carti.jpg" },
+    { id: 2, nome: "Niink", img: "/niink.jpg" },
+    { id: 3, nome: "Yunk-vino", img: "/vino.jpg" },
+    { id: 4, nome: "Jé Santiago", img: "/je.jpg" },
+  ]
+
+  const albuns = [
+    {id: 1, nome:  'MUSIC', artista: "Playboi Carti", img: '/MUSIC.jpg'},
+    {id: 2, nome:  'Calzone tapes 3', artista: "Recayd Mob", img: '/calzone.jpg'},
+    {id: 3, nome:  'Um Quebrada Inteligente', artista: "Kyan, DJ MU540", img: '/quebrada.jpeg'},
+    {id: 4, nome:  'SEMRÉH', artista: "Ryu, the runner", img: '/semreh.jpeg'},
+    {id: 5, nome:  'Ainda no mesmo bairro', artista: "Niink", img: '/nink.jpeg'},
+    {id: 6, nome:  'Menino bom, mlk ruim', artista: "Rudies Flacko", img: '/rudies.jpeg'},
+  ]
+
+  const faixas = [
+    {id: 1, musica: 'RATHER LIE', artista: "Playboi Carti", img: '/MUSIC.jpg'},
+    {id: 2, musica: 'Numa Balada Qualquer', artista: "Niink", img: '/nink.jpeg'},
+    {id: 3, musica: 'Fui Mlk', artista: "Paiva", img: '/paiva.jpeg'},
+    {id: 4, musica: 'Os Mlks é Ruim', artista: "Mc Negão Original, Veigh", img: '/mcnegao.jpeg'},
+    {id: 5, musica: 'Mente do Coringa', artista: "CJota, Ghard, Ryu, the runner", img: '/cjota.jpeg'},
+  ]
+
+
   return (
     <>
       <section id="banner">
@@ -19,10 +45,9 @@ export default function Home() {
         <h1 className="font-extrabold text-4xl mt-6 hover:text-red-600 duration-300 flex justify-center">Artistas principais</h1>
         <div className="flex justify-center mt-5">
           <div className="artistas">
-            <img src="/carti.jpg"></img>
-            <img src="/niink.jpg"></img>
-            <img src="/vino.jpg"></img>
-            <img src="/je.jpg"></img>
+            {artistas.map((artista) => (
+              <img src={artista.img}></img>
+            ))}
           </div>
         </div>
       </section>
@@ -30,76 +55,29 @@ export default function Home() {
       <section id="album">
         <h1 className="font-extrabold text-4xl mt-6 hover:text-red-600 duration-300 ml-15">Albums principais</h1>
         <div className="albums flex justify-evenly mt-10">
-
-          <div className="album-list hover:scale-110 duration-300 cursor-pointer">
-            <img className="rounded-3xl mb-1" src="/MUSIC.jpg" width={250}></img>
-            <p>MUSIC</p>
-            <p className="text-gray-500">Playboi Carti</p>
+          {albuns.map((album) => (
+            <div  className="album-list hover:scale-110 duration-300 cursor-pointer">
+            <img className="rounded-3xl mb-1" src={album.img} width={250}></img>
+            <p>{album.nome}</p>
+            <p className="text-gray-500">{album.artista}</p>
           </div>
-          <div className="album-list hover:scale-110 duration-300 cursor-pointer">
-            <img className="rounded-3xl mb-1" src="/calzone.jpg" width={250}></img>
-            <p>Calzone tapes 3</p>
-            <p className="text-gray-500">Recayd Mob</p>
-          </div>
-          <div className="album-list hover:scale-110 duration-300 cursor-pointer">
-            <img className="rounded-3xl mb-1" src="/quebrada.jpeg" width={250}></img>
-            <p>Um quebrada inteligente</p>
-            <p className="text-gray-500">Kyan e MU54O</p>
-          </div>
-          <div className="album-list hover:scale-110 duration-300 cursor-pointer">
-            <img className="rounded-3xl mb-1" src="/semreh.jpeg" width={250}></img>
-            <p>SEMRÉH</p>
-            <p className="text-gray-500">Ryu, the runner</p>
-          </div>
-          <div className="album-list hover:scale-110 duration-300 cursor-pointer">
-            <img className="rounded-3xl mb-1" src="/nink.jpeg" width={250}></img>
-            <p>Ainda no mesmo bairro</p>
-            <p className="text-gray-500">Niink</p>
-          </div>
-          <div className="album-list hover:scale-110 duration-300 cursor-pointer">
-            <img className="rounded-3xl mb-1" src="/rudies.jpeg" width={250}></img>
-            <p>Menino bom, mlk ruim</p>
-            <p className="text-gray-500">Rudies Flacko</p>
-          </div>
+          ))}
         </div>
       </section>
 
       <section id="faixa">
         <h1 className="font-extrabold text-3xl m-10">Faixas mais ouvidas</h1>
-
         <table className="ml-10 w-[50%]">
-          <tbody className="p-10 rounded-4xl">
+          {faixas.map((faixa) => (
+            <tbody className="p-10 rounded-4xl">
             <tr className="border-b-3 hover:bg-[#11161a] duration-400">
-              <td className="py-3">1</td>
-              <td className="py-3"><img src="/MUSIC.jpg" alt="Música" width={50} /></td>
-              <td className="py-3">MUSIC<p>Playboi Carti </p></td>
-              <td className="py-3"> <p className="ml-5">Avaliação aqui</p></td>
-            </tr>
-            <tr className="border-b-3 hover:bg-[#11161a] duration-400">
-              <td className="py-3">2</td>
-              <td className="py-3"><img src="/nink.jpeg" alt="Música" width={50} /></td>
-              <td className="py-3">Numa Balada Qualquer<p>Niink </p></td>
-              <td className="py-3"> <p className="ml-5">Avaliação aqui</p></td>
-            </tr>
-            <tr className="border-b-3 hover:bg-[#11161a] duration-400">
-              <td className="py-3">3</td>
-              <td className="py-3"><img src="/paiva.jpeg" alt="Música" width={50} /></td>
-              <td className="py-3">Fui Mlk<p>Mc Paiva </p></td>
-              <td className="py-3"> <p className="ml-5">Avaliação aqui</p></td>
-            </tr>
-            <tr className="border-b-3 hover:bg-[#11161a] duration-400">
-              <td className="py-3">4</td>
-              <td className="py-3"><img src="/mcnegao.jpeg" alt="Música" width={50} /></td>
-              <td className="py-3">Os Mlks é Ruim<p>Mc Negão Original, Veigh </p></td>
-              <td className="py-3"> <p className="ml-5">Avaliação aqui</p></td>
-            </tr>
-            <tr className="hover:bg-[#11161a] duration-400">
-              <td className="py-3">5</td>
-              <td className="py-3"><img src="/cjota.jpeg" alt="Música" width={50} /></td>
-              <td className="py-3">Mente do Coringa<p>CJota, Ryu, the runner, Ghard </p></td>
-              <td className="py-3"> <p className="ml-5">Avaliação aqui</p></td>
+              <td className="py-3">{faixa.id}</td>
+              <td className="py-3"><img src={faixa.img} alt="Música" width={50} /></td>
+              <td className="py-3">{faixa.musica}<p>{faixa.artista}</p></td>
+              <td className="py-3"> <p className="ml-5"></p></td>
             </tr>
           </tbody>
+      ))}
         </table>
       </section>
 
